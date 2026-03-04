3a615372b00dcf9a3b0f57bf8856d9c4836b8c45
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-03-04 13:07:24 +0530

    feat: enhance cookie security settings for user authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -40,7 +40,14 @@ const registerUserController = async (req, res) => {
             { expiresIn: '7d' }
         )
 
-        res.cookie("token", token)
+        // res.cookie("token", token)
+
+        res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+})
 
         res.status(201).json({
             message: "User registered successfully",
@@ -87,7 +94,14 @@ const loginUserController = async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: '7d' }
         )
-        res.cookie("token", token)
+        //res.cookie("token", token)
+
+        res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+})
 
         res.status(200).json({
             message: "User loggedIn successfully",
@@ -116,7 +130,13 @@ const logoutUserController = async (req, res) => {
             await tokenBlacklistModel.create({ token })
         }
 
-        res.clearCookie("token")
+       // res.clearCookie("token")
+
+       res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
 
         res.status(200).json({
             message: "User logged out successfully"
